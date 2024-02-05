723b95bac8cbb1e47e5eb229a32447876e904b6c
Khoirulez <123630353+Khoirulez@users.noreply.github.com>
2024-02-05 17:44:07 +0700

    Add files via upload

--- RR07.py
+++ RR07.py
@@ -243,20 +243,20 @@ def info_user():
 #--------------------[ BAGIAN-MASUK ]--------------#
 def login():
     try:
-	   token = open('.token.txt','r').read()
-	   cok = open('.cok.txt','r').read()
-	   tokenku.append(token)
-	   except (IOError,KeyError,FileNotFoundError):
-		   print('[×] Cookies Kadaluarsa ')
-		   time.sleep(5)
-		   login_lagi334()
+	    token = open('.token.txt','r').read()
+	    cok = open('.cok.txt','r').read()
+	    tokenku.append(token)
+    except (IOError,KeyError,FileNotFoundError):
+		print('[×] Cookies Kadaluarsa ')
+		time.sleep(5)
+		login_lagi334()
 	   try:
 		   info_datafb = ses.get(f"https://graph.facebook.com/me?fields=name,id&access_token={token}", cookies = {'cookies':cok}).json()
 		   sy2 = info_datafb["name"]
 		   sy3 = info_datafb["id"]
 		   login(sy2,sy3)
 	   except requests.exceptions.ConnectionError:
-		   exit(f"\n{P} [:] Tidak ada koneksi{P}")
+		   exit(f"\n{P} [:]{M} Tidak ada koneksi{P}")
 	except KeyError:
 		try:os.remove(".cok.txt");os.remove(".token.txt")
 		except:pass
