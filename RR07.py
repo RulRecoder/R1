b9818eb4fad18f53eccb5845a3c8b65fa1754c43
Khoirulez <123630353+Khoirulez@users.noreply.github.com>
2024-02-08 16:55:43 +0700

    Add files via upload

--- RR07.py
+++ RR07.py
@@ -678,8 +678,7 @@ def massal():
 		exit()
 ###----------[ ATUR SBLUM KREK ]----------###
 def setting():
-        prints(nel(f'                  {H2}{len(id)}[bold blue] ID TELAH DIKUMPULKAN'))
-	prints(nel(f'                        [bold blue]login ID Crack')) 
+	cetak(nel(f"                        [bold blue]login ID Crack",title=f"{H2}{len(id)}{P2} ID TELAH DIKUMPULKAN",width=70,padding=(0,7),style=f"{color_panel}"))
 	print(f"{H2}╰─▶ {B2}01 [bold purple]Facebook ID {M2}Old\n")
 	print(f"{H2}╰─▶ {B2}02 [bold purple]Facebook ID {K2}New\n")
 	print(f"{H2}╰─▶ {B2}03 [bold purple]Facebook ID {H2}Random\n")
