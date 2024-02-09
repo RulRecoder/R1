5a97674e98c7c4114243badda243e16a06475844
Khoirulez <123630353+Khoirulez@users.noreply.github.com>
2024-02-09 10:47:22 +0700

    Update RR07.py

--- RR07.py
+++ RR07.py
@@ -939,7 +939,7 @@ def passwrd():
 	print(nel(f'               !{H} ON/OFF MODE PESAWAT SEBELUM MULAI{N} !', style=f"bold purple"))
 	with prog:
 	     with tred(max_workers=30) as pool:
-		  for yuzong in id2:
+		    for yuzong in id2:
 			idf,nmf = yuzong.split('|')[0],yuzong.split('|')[1].lower()
 			frs = nmf.split(' ')[0]
 			pwv = []
