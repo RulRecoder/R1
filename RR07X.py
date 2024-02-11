d96fdd2792ba65e521fdf53e6a7634d0be711176
Khoirulez <123630353+Khoirulez@users.noreply.github.com>
2024-02-11 14:08:15 +0700

    Add files via upload

--- RR07X.py
+++ RR07X.py
@@ -643,7 +643,7 @@ class Furthermore:
                if "c_user" in ses.cookies.get_dict():
                   kuki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items() ])
                   tree = Tree(f"\r",guide_style="bold grey100")
-                  tree.add(f"{H2}  ⫸{P2} {user}\n {H2}  ⫸{P2} {pw}\n").add(f'{P2}  ╰─▶ {{H2}{kuki}\n {P2}  ╰─▶{U2}{ua}{x}')
+                  tree.add(f"{H2}  ⫸{P2} {user}\n {H2}  ⫸{P2} {pw}\n").add(f'{P2}  ╰─▶ {H2}{kuki}\n {P2}  ╰─▶{U2}{ua}{x}')
                   prints(tree)
                   kntl = (f"{user}|{pw}|{kuki}")
                   self.live.append(kntl)
@@ -710,7 +710,7 @@ class Furthermore:
                if "c_user" in ses.cookies.get_dict():
                   kuki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items() ])
                   tree = Tree(f"\r",guide_style="bold grey100")
-                  tree.add(f"{H2}  ⫸{P2} {user}\n {H2}  ⫸{P2} {pw}\n").add(f'{P2}  ╰─▶ {{H2}{kuki}\n {P2}  ╰─▶{U2}{ua}{x}')
+                  tree.add(f"{H2}  ⫸{P2} {user}\n {H2}  ⫸{P2} {pw}\n").add(f'{P2}  ╰─▶ {H2}{kuki}\n {P2}  ╰─▶{U2}{ua}{x}')
                   prints(tree)
                   kntl = (f"{user}|{pw}|{kuki}")
                   self.live.append(kntl)
@@ -792,7 +792,7 @@ class Furthermore:
                if "c_user" in ses.cookies.get_dict():
                   kuki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items() ])
                   tree = Tree(f"\r",guide_style="bold grey100")
-                  tree.add(f"{H2}  ⫸{P2} {user}\n {H2}  ⫸{P2} {pw}\n").add(f'{P2}  ╰─▶ {{H2}{kuki}\n {P2}  ╰─▶{U2}{ua}{x}')
+                  tree.add(f"{H2}  ⫸{P2} {user}\n {H2}  ⫸{P2} {pw}\n").add(f'{P2}  ╰─▶ {H2}{kuki}\n {P2}  ╰─▶{U2}{ua}{x}')
                   prints(tree)
                   kntl = (f"{user}|{pw}|{kuki}")
                   self.live.append(kntl)
@@ -863,7 +863,7 @@ class Furthermore:
                if "c_user" in ses.cookies.get_dict():
                   kuki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items() ])
                   tree = Tree(f"\r",guide_style="bold grey100")
-                  tree.add(f"{H2}  ⫸{P2} {user}\n {H2}  ⫸{P2} {pw}\n").add(f'{P2}  ╰─▶ {{H2}{kuki}\n {P2}  ╰─▶{U2}{ua}{x}')
+                  tree.add(f"{H2}  ⫸{P2} {user}\n {H2}  ⫸{P2} {pw}\n").add(f'{P2}  ╰─▶ {H2}{kuki}\n {P2}  ╰─▶{U2}{ua}{x}')
                   prints(tree)
                   kntl = (f"{user}|{pw}|{kuki}")
                   self.live.append(kntl)
